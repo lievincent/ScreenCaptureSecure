5ab7ad1fc2cc1c65d3b8598c492cacfcbc159427
Vincent Lie <48110686+lievincent@users.noreply.github.com>
2022-04-28 12:55:59 +0700

    Update ScreenCaptureSecure.m
    change value of the alert

--- ios/ScreenCaptureSecure.m
+++ ios/ScreenCaptureSecure.m
@@ -21,11 +21,11 @@ RCT_EXPORT_METHOD(enableSecure)
                                                     queue:mainQueue
                                                 usingBlock:^(NSNotification *note) {
                                                     // executes after screenshot
-                                                    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"주의"
-                                                                                message:@"작품을 캡쳐한 스크린샷을\n온라인, 오프라인에 무단으로\n공유 및 배포할 경우 법적인\n제재를 받을 수 있습니다."
+                                                    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Warning"
+                                                                                message:@"Screen capture detected.\n Please don't take any pictures from this screen"
                                                                                 preferredStyle:UIAlertControllerStyleAlert];
 
-                                                    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"확인" style:UIAlertActionStyleDefault
+                                                    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
                                                     handler:^(UIAlertAction * action) {}];
 
                                                     [alert addAction:defaultAction];
